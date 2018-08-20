105710eb807eda199d909052d2fb2ad3eb1e13b7
Jack <jackdh@users.noreply.github.com>
2018-08-20 09:54:11 +0100

    Added information for generate response.

--- server/mongo/direct/generateResponse.js
+++ server/mongo/direct/generateResponse.js
@@ -7,6 +7,17 @@ const debug = require('debug')('GenerateResponse:');
 const History = require('../controllers/message_history');
 const { parseInteral } = require('../controllers/training');
 
+/**
+ * It will use co as a generator to go off and concurrently add the message to
+ * the users history, get the dialog flow for this agent, parse the request
+ * against the NLU engine and finally get the users session or create
+ * it if it does not exist
+ *
+ * @param uid - The unique ID for this conversation
+ * @param message - the humans message
+ * @param project - Which agent should we target.
+ * @param model - Which model within the agent should we target.
+ */
 function generateResponseInternal(uid, message, project, model) {
   return new Promise(resolve =>
     co(function* t() {
@@ -38,6 +49,9 @@ function generateResponseInternal(uid, message, project, model) {
   );
 }
 
+/**
+* This is a simple wrapper function for the above generate response. It exposes the main generate response method to API calls.
+*/
 function generateResponse(req, res) {
   const { uid } = req.params;
   const { message, project, model } = req.body;
