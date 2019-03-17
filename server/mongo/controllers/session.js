56569e0942a5d9db1107fa08aab35c570f9fb902
Jack <jackdh@users.noreply.github.com>
2019-03-17 16:46:47 +0000

    Updated session controller to use generators.

--- server/mongo/controllers/session.js
+++ server/mongo/controllers/session.js
@@ -2,36 +2,34 @@
 // const db = require('../mongo/mongodb');
 const SessionSchema = require('../schemas/sessionSchema');
 const debug = require('debug')('session');
+const co = require('co');
 
 /**
  * Gets the session or creates one.
  * @param userID
  * @returns {Promise}
  */
-function getSessionInternal(userID) {
-  return new Promise(resolve => {
-    SessionSchema.findOne({ sessionID: userID })
-      .then(model => {
-        if (model) {
-          if (!model.entities.saved) {
-            model.entities.saved = {};
-          }
-          resolve(model);
-        } else {
-          SessionSchema.create({ sessionID: userID }).then(m => {
-            m.entities = {
-              context: {},
-              saved: {},
-            };
-            return resolve(m);
-          });
-        }
-      })
-      .catch(error => {
-        debug(error);
-      });
-  });
-}
+const getSessionInternal = userID =>
+  co(function* t() {
+    const model = yield SessionSchema.findOne({ sessionID: userID });
+
+    if (model) {
+      if (!model.entities.saved) {
+        model.entities.saved = {};
+      }
+      return model;
+    }
+    const newModel = yield SessionSchema.create({ sessionID: userID });
+    newModel.entities = {
+      context: {},
+      saved: {},
+    };
+    return newModel;
+  })
+    .then(data => data)
+    .catch(error => {
+      debug(error);
+    });
 
 module.exports = {
   getSessionInternal,
